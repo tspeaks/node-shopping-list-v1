2878c4c3c3db3a7300a06707cd43b71f62ca98d1
Tyler Speaks <speaks_tyler@yahoo.com>
2017-08-10 19:51:37 -0500

    Added Recipes for lesson

--- server.js
+++ server.js
@@ -18,11 +18,21 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+//adding Recipes to retrieve
+Recipes.create(
+  'PB&J Sandwich', ['2 slices of bread', '1 Tbsp Jelly', '2 Tbsp Peanut Butter']);
+Recipes.create(
+  'Turkey/Cheese Sandwich', ['2 slices of bread', '2 slices of Cheese', '3 slices of Turkey']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
